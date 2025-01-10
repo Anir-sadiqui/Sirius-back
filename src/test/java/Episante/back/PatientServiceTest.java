b032720029f2116e5023f0bc2186527ac7ff6b5f
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-01-10 17:04:50 +0100

    feat: Implement password encoding and unit tests for PatientService

--- src/test/java/Episante/back/PatientServiceTest.java
+++ src/test/java/Episante/back/PatientServiceTest.java
@@ -1,8 +1,8 @@
 package Episante.back;
 
 
-import Episante.back.Repository.IPatientDao;
-import Episante.back.Models.Patient ;
+import Episante.back.DAO.IPatientDao;
+import Episante.back.Entity.Patient ;
 import Episante.back.Service.PatientService ;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,6 +11,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
