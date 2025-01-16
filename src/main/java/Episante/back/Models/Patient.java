54f8a419b5bb03d9b6f6489f1d365ccea27309b5
Anir-sadiqui <anirsadiqui2@gmail.com>
2025-01-16 17:26:30 +0100

    Entity

--- src/main/java/Episante/back/Models/Patient.java
+++ src/main/java/Episante/back/Models/Patient.java
@@ -1,18 +1,25 @@
 package Episante.back.Models;
 
-import Episante.back.Models.RendezVous;
 import jakarta.persistence.*;
+import lombok.Data;
 
-import java.util.List;
-
+@Data
 @Entity
 public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
     private String nom;
     private String prenom;
+    private int age;
+    private String adresse;
+    private String email;
+    private String telephone;
+    private String taille;
+    private Sexe sexe;
+    @OneToOne(mappedBy = "patient", cascade = CascadeType.ALL, orphanRemoval = true)
+    private H_Medical h_m;
 
-    @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL)
-    private List<RendezVous> rdvs;
+
+
 }
--- src/main/java/Episante/back/Repository/IPatientrepository.java
+++ src/main/java/Episante/back/Repository/IPatientrepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface IPatientrepository extends org.springframework.data.jpa.repository.JpaRepository<Patient, Long> {
+public interface IPatientrepository extends ICrudRepository<Patient, Long> {
 
     List<Patient> findByNom(String name) ;
 
