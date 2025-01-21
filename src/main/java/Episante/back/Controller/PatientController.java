83b0bb9de6905314d400c7b110dd818e375f791b
Anir-sadiqui <anirsadiqui2@gmail.com>
2025-01-21 21:17:59 +0100

    update

--- src/main/java/Episante/back/Controller/PatientController.java
+++ src/main/java/Episante/back/Controller/PatientController.java
@@ -2,6 +2,7 @@ package Episante.back.Controller;
 
 import Episante.back.Repository.IPatientrepository;
 import Episante.back.Models.Patient;
+import Episante.back.Service.PatientService;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,21 +25,21 @@ public class PatientController {
 
 
     @Autowired
-    private IPatientrepository patientDao;
+    private PatientService patientService;
 
     @GetMapping
     public List<Patient> getAllPatients() {
         logger.info("Get all patients");
-        return patientDao.findAll();
+        return patientService.getAllPatients();
     }
 
 
     //using the ResponseEntity to see the status of the request
     @PostMapping
     public ResponseEntity<Patient> createPatient(@Valid @RequestBody Patient patient) {
-        Patient savedPatient = patientDao.save(patient);
-        logger.info("Patient created successfully: {}", savedPatient.getId());
-        return ResponseEntity.status(HttpStatus.CREATED).body(savedPatient);
+        patientService.addPatient(patient);
+        logger.info("Patient created successfully: {}", patient.getNom() + " " + patient.getPrenom());
+        return ResponseEntity.status(HttpStatus.CREATED).body(patient);
     }
 
 // old version
@@ -58,7 +59,7 @@ public class PatientController {
 //    }
     @PutMapping("/{id}")
     public ResponseEntity<Patient> updatePatient(@PathVariable Long id , @Valid @RequestBody Patient patientDetails) {
-        Patient patient = patientDao.findById(id).orElseThrow(() -> new RuntimeException("Patient not found with id: " + id));
+        Patient patient = patientService.findById(id).orElseThrow(() -> new RuntimeException("Patient not found with id: " + id));
         BeanUtils.copyProperties(patientDetails, patient); // with copyProperties method you just copy the propreties of your Patient to avoid setters
         Patient updatedPatient = patientDao.save(patient);
         logger.info("Patient updated successfully: {}", updatedPatient.getId());
