6c86af18246d7036fe1f98ef97b1827ec33a2de6
IKRAMELHAMI <ikramelhami02@gmail.com>
2025-01-23 17:42:30 +0100

    Feature: gérer les disponibilitées

--- src/main/java/Episante/back/Models/Patient.java
+++ src/main/java/Episante/back/Models/Patient.java
@@ -1,66 +1,18 @@
 package Episante.back.Models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import Episante.back.Models.RendezVous;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String nom;
     private String prenom;
-    private int age;
-    private double poids;
-    private Sexe sexe;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public double getPoids() {
-        return poids;
-    }
-
-    public void setPoids(double poids) {
-        this.poids = poids;
-    }
-
-    public Sexe getSexe() {
-        return sexe;
-    }
-
-    public void setSexe(Sexe sexe) {
-        this.sexe = sexe;
-    }
+    @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL)
+    private List<RendezVous> rdvs;
 }
--- src/main/java/Episante/back/Repository/IPatientDao.java
+++
@@ -1,7 +0,0 @@
-package Episante.back.Repository;
-
-import Episante.back.Models.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IPatientDao extends JpaRepository<Patient, Long> {
-}
