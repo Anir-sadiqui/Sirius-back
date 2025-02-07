660b0415cdb1ed7b41aca45aa9cbcf9affdfc4ac
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-02-07 10:29:59 +0100

    Merge with IKRAM branch and Fin the conflict

--- src/main/java/Episante/back/Repository/INotificationDao.java
+++
@@ -1,7 +0,0 @@
-package Episante.back.Repository;
-
-import Episante.back.Models.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface INotificationDao extends JpaRepository<Notification, Long> {
-}
